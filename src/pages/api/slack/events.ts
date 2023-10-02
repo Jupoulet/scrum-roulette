6602b37103ccd088cd5d55156af9020bc093aaf5
Jupoulet <julien.pierre.picard@gmail.com>
2023-10-02 18:58:32 +0200

    wip

--- src/pages/api/slack/events.ts
+++ src/pages/api/slack/events.ts
@@ -5,10 +5,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ message: string }>
 ) {
+  console.log('BONSOIR');
   if (req.method !== 'POST') {
-    return res.status(404);
+    return res.status(404).json({ message: 'Not found' });
   }
   const payload = req.body;
   await postSlackMessage(`Hello moi: ${payload}`)
-  res.status(200);
+  res.status(200).json({ message: 'Ok' });
 }
