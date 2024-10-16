33713b63f0b2c4ee6137f5d79980583d071c80b6
Jupoulet <julien.pierre.picard@gmail.com>
2024-10-16 11:35:30 +0200

    Add custom messages for assignees

--- src/pages/api/deps_owner_roulette.ts
+++ src/pages/api/deps_owner_roulette.ts
@@ -75,8 +75,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ message: string }>
 ) {
-
   const message = await depsOwnerRouletteScript(req.body.author);
-  
   res.status(200).json({ message });
 };
